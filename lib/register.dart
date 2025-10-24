7fcb67299e7720e727048c0b1cb2e8cb60330274
Shruti Sakshi <156585094+shrutisakshi08@users.noreply.github.com>
2025-10-24 18:05:07 +0530

    Add registration screen with user input fields
    This file implements a registration screen with fields for name, email, and password, along with sign-up and sign-in buttons.

---
+++ lib/register.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({super.key});
+
+  @override
+  MyRegisterState createState() => MyRegisterState();
+}
+
+class MyRegisterState extends State<MyRegister> {
+  final TextEditingController _passwordTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown,
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 35, top: 100),
+            child: Text(
+              'Create\nAccount',
+              style: TextStyle(color: Color(0xFFF8F8F8), fontSize: 33),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.38,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 35, right: 35),
+                    child: Column(
+                      children: [
+                        // Name TextField
+                        TextField(
+                          style: TextStyle(color: Color(0xFFF8F8F8)),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Color(0xFFF8F8F8)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.brown),
+                            ),
+                            hintText: "Name",
+                            hintStyle: TextStyle(color: Color(0xFFF8F8F8)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Color(0xFFF8F8F8),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+
+                        // Email
+                        TextField(
+                          controller: _emailTextController,
+                          style: TextStyle(color: Color(0xFFF8F8F8)),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Color(0xFFF8F8F8)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.brown),
+                            ),
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Color(0xFFF8F8F8)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Color(0xFFF8F8F8),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+
+                        // Password
+                        TextField(
+                          controller: _passwordTextController,
+                          style: TextStyle(color: Color(0xFFF8F8F8)),
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Color(0xFFF8F8F8)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.brown),
+                            ),
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Color(0xFFF8F8F8)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Color(0xFFF8F8F8),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 40),
+
+                        // Sign up button
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                color: Color(0xFFF8F8F8),
+                                fontSize: 27,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 30,
+                              backgroundColor: Color(0xFFF8F8F8),
+                              child: IconButton(
+                                color: Colors.brown,
+                                onPressed: () {},
+                                icon: Icon(Icons.arrow_forward),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 40),
+
+                        // Sign in button
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, 'login');
+                              },
+                              style: ButtonStyle(),
+                              child: Text(
+                                'Sign In',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Color(0xFFF8F8F8),
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
