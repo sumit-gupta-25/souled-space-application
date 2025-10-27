adf2c30e7227c8254433a75608f3fb547f597cbc
MisLead07 <mohammad13082004@gmail.com>
2025-10-27 14:03:25 +0530

    Done finally

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:firebase_core/firebase_core.dart';
-=======
-import 'package:souled_space_application/home.dart';
-import 'package:souled_space_application/login.dart';
-import 'package:souled_space_application/register.dart';
->>>>>>> 83243f9a2dd2f9761b1075ccf31f1494b18e889d
 import 'package:souled_space_application/group/group_home.dart';
 import 'package:souled_space_application/home.dart';
 import 'package:souled_space_application/individual/anonymous_venting_wall.dart';
-<<<<<<< HEAD
 import 'package:souled_space_application/individual/indi_home.dart';
 import 'package:souled_space_application/individual/journaling.dart';
 import 'package:souled_space_application/individual/stress_thermometer.dart';
@@ -22,11 +15,6 @@ Future<void> main() async {
 
   // ✅ Initialize Firebase before running the app
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-=======
-import 'package:souled_space_application/individual/stress_thermometer.dart';
-import 'package:souled_space_application/individual/indi_home.dart';
-import 'package:souled_space_application/individual/journaling.dart';
->>>>>>> 83243f9a2dd2f9761b1075ccf31f1494b18e889d
 
   runApp(const MainApp());
 }
