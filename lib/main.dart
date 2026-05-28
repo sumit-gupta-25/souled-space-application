aa3cca5d9f5b15812eddd48909f1c71280d23ba2
MisLead07 <mohammad13082004@gmail.com>
2026-05-28 13:59:50 +0530

    Removed dotenv dependency from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:souled_space_application/group/group_home.dart';
 import 'package:souled_space_application/home.dart';
 import 'package:souled_space_application/individual/journaling.dart';
